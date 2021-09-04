a490ebae9ea1d303f8edf37f86bc0a98076aac99
Lenfumé Julien <julabina@hotmail.fr>
2021-09-04 20:22:30 +0200

    04 09 21 20 22

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const sunriseCurrentContainer = document.querySelector(".sunrise");
-const sunsetCurrentContainer = document.querySelector(".sunset");
-
-let trainel, paris, sunrise, sunset;
+let trainel, paris, sunrise, sunset, dt, today;
 
 fetch(
   "https://api.openweathermap.org/data/2.5/onecall?lat=48.41&lon=3.45&units=metric&appid=f0bc558c25b59fd4f095d9f96b29fb6c"
@@ -23,45 +20,65 @@ fetch(
 const convertTimeCurrent = (city) => {
   let sunriseTimestamp = city.current.sunrise;
   let sunsetTimestamp = city.current.sunset;
+  let dtTimestamp = city.current.dt;
+  dt = new Date(dtTimestamp * 1000);
   sunrise = new Date(sunriseTimestamp * 1000);
   sunset = new Date(sunsetTimestamp * 1000);
 };
 
 const Displaytest = (city) => {
-  const gpsCurrent = document.querySelector(".gpsLoca");
+  convertTimeCurrent(city);
+  const currentSunriseContainer = document.querySelector(".sunrise");
+  const currentSunsetContainer = document.querySelector(".sunset");
   const currentDt = document.querySelector(".currentTime");
-  const imgWeather = document.querySelector(".weatherImg");
-  let dtTimestamp = city.current.dt;
-  let dt = new Date(dtTimestamp * 1000);
-  let day;
+  const currentImgWeather = document.querySelector(".weatherImg");
+  const CurrentDescriptionWeather = document.querySelector(
+    ".weatherDescription"
+  );
+  const currentTemp = document.querySelector(".temperature");
+  const currentFeeling = document.querySelector(".feeling");
+  const currentHumidity = document.querySelector(".humidity");
+  const currentCloudiness = document.querySelector(".cloudiness");
+  const currentUVIndex = document.querySelector(".UVIndex");
+
+  let tempRounded, feelRounded;
+
   let dayStamp = dt.getDay();
+
   if (dayStamp === 0) {
   } else if (dayStamp === 1) {
-    day = "Monday";
+    today = "Monday";
   } else if (dayStamp === 2) {
-    day = "Tuesday";
+    today = "Tuesday";
   } else if (dayStamp === 3) {
-    day = "Wednesday";
+    today = "Wednesday";
   } else if (dayStamp === 4) {
-    day = "Thursday";
+    today = "Thursday";
   } else if (dayStamp === 5) {
-    day = "Friday";
+    today = "Friday";
   } else if (dayStamp === 6) {
-    day = "Saturday";
+    today = "Saturday";
   } else {
-    day = "Sunday";
+    today = "Sunday";
   }
-  currentDt.textContent = dt.getHours() + "h" + dt.getMinutes() + "  " + day;
-  sunriseCurrentContainer.textContent =
+
+  currentDt.textContent = dt.getHours() + "h" + dt.getMinutes() + "  " + today;
+  currentSunriseContainer.textContent =
     "Sunrise : " + sunrise.getHours() + "h" + sunrise.getMinutes();
-  sunsetCurrentContainer.textContent =
+  currentSunsetContainer.textContent =
     "Sunset  : " + sunset.getHours() + "h" + sunset.getMinutes();
-  test = city.current.weather[0].icon;
+  currentIcon = city.current.weather[0].icon;
   description = city.current.weather[0].description;
-  console.log(test);
-  test2 = "http://openweathermap.org/img/wn/" + test + "@2x.png";
-  console.log(description);
-  imgWeather.innerHTML = `
-  <img src="${test2}" alt="${description}">
+  imgCurrent = "http://openweathermap.org/img/wn/" + currentIcon + "@2x.png";
+  currentImgWeather.innerHTML = `
+  <img src="${imgCurrent}" alt="${description}">
   `;
+  CurrentDescriptionWeather.textContent = city.current.weather[0].description;
+  tempRounded = Math.round(city.current.temp);
+  feelRounded = Math.round(city.current.feels_like);
+  currentTemp.textContent = tempRounded;
+  currentFeeling.textContent = feelRounded;
+  currentHumidity.textContent = city.current.humidity + "%";
+  currentCloudiness.textContent = city.current.clouds + "%";
+  currentUVIndex.textContent = city.current.uvi;
 };
